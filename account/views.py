295babde4410b3c089b124a693eaac8948173d23
Ansah-98 <yungtupac99@gmail.com>
2024-10-14 04:20:31 +0000

    login view done

--- account/views.py
+++ account/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+from .forms import LoginForm
+from django.contrib.auth import login, authenticate 
+
+
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd  = form.cleaned_data
+            user = authenticate(username = cd.username, 
+                                password = cd.password)
+            if user is not None:
+                if user.is_active:
+                    login(user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('User has been deactivated')
+        return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request,'account/login',{'form':form})

b63c0e3190c423678e23936f0d9fe2340d5b71ed
Ansah-98 <yungtupac99@gmail.com>
2024-10-14 04:42:44 +0000

    updating the login view with features

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,6 @@
 from django import forms 
 
 
-class LoginForm(forms.Forms):
+class LoginForm(forms.Form):
     username  = forms.CharField()
     password  = forms.CharField(widget=forms.PasswordInput)
